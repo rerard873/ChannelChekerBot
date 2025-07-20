7351cd78cc9936cd88f84774826c4de56b785960
odincovm <odincovmisa16@gmail.com>
2025-07-20 11:35:41 +0300

    Init

---
+++ src/main/java/io/ChBot/ChannelChekerBot/ChannelChekerBotApplication.java
@@ -0,0 +1,13 @@
+package io.ChBot.ChannelChekerBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannelChekerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannelChekerBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ChBot/ChannelChekerBot/ChannelChekerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ChBot.ChannelChekerBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChannelChekerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
